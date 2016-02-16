97675fd787f54119146f32b4a9ad7b1f1b1be7fc
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-02-16 20:30:22 +0100

    Update directed_graph.cpp

--- src/directed_graph.cpp
+++ src/directed_graph.cpp
@@ -1,3 +1,17 @@
 #include "directed_graph.hpp"
 
+unsigned int min_index( std::set<vertex> queue , int dist[] , unsigned int n )
+{
+	int imin = (*queue.begin()).get_label() ;
+	
+	for ( auto i = queue.begin() ; i!= queue.end() ; ++i )
+	{
+		if ( dist[(*i).get_label()] < dist[imin] )
+		{
+			imin= (*i).get_label();
+		}
+	}
+	return imin;
+} 
+
 template class directed_graph<int> ;
